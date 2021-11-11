e13be53e559ba8dd50cb8e56d377d75aaa58e408
SireInsectus <SireInsectus>
2021-11-11 22:47:10 +0000

    Checkpoint

---
+++ Example-Course-vTEST/Agenda.py
@@ -0,0 +1,47 @@
+# Databricks notebook source
+# MAGIC %md-sandbox
+# MAGIC 
+# MAGIC <div style="text-align: center; line-height: 0; padding-top: 9px;">
+# MAGIC   <img src="https://databricks.com/wp-content/uploads/2018/03/db-academy-rgb-1200px.png" alt="Databricks Learning" style="width: 600px">
+# MAGIC </div>
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC We are not really doing anything special here short of documenting how to use replacements and showing off dbgems
+# MAGIC <pre>
+# MAGIC * AWS: {{AWS}}
+# MAGIC * username: {{username}}
+# MAGIC * spark_version: {{spark_version}}
+# MAGIC * instance_pool_id: {{instance_pool_id}}
+# MAGIC * host_name: {{host_name}}
+# MAGIC * username: {{username}}
+# MAGIC * notebook_path: {{notebook_path}}
+# MAGIC * notebook_dir: {{notebook_dir}}
+# MAGIC * api_endpoint: {{api_endpoint}}
+# MAGIC * api_token: {{api_token}}
+# MAGIC </pre>
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC # Here is a quick index
+# MAGIC ## Lessons
+# MAGIC * [Your First Lesson]($./EC 01 - Your First Lesson)
+# MAGIC * [Your Second Lesson]($./EC 02 - Your Second Lesson)
+# MAGIC * [Your Third Lesson]($./EC 03 - Your Third Lesson)
+# MAGIC * [Your Fourth Lesson]($./EC 04 - Your Fourth Lesson)
+# MAGIC 
+# MAGIC ## Labs
+# MAGIC * [Your First Lab]($./Labs/EC 01L - Your First Lab)
+# MAGIC * [Your Second Lab]($./Labs/EC 02L - Your Second Lab)
+# MAGIC * [Your Third Lab]($./Labs/EC 03L - Your Third Lab)
+# MAGIC * [Your Fourth Lab]($./Labs/EC 04L - You Fourth Lab)
+
+# COMMAND ----------
+
+# MAGIC %md-sandbox
+# MAGIC &copy; 2021 Databricks, Inc. All rights reserved.<br/>
+# MAGIC Apache, Apache Spark, Spark and the Spark logo are trademarks of the <a href="http://www.apache.org/">Apache Software Foundation</a>.<br/>
+# MAGIC <br/>
+# MAGIC <a href="https://databricks.com/privacy-policy">Privacy Policy</a> | <a href="https://databricks.com/terms-of-use">Terms of Use</a> | <a href="http://help.databricks.com/">Support</a>
